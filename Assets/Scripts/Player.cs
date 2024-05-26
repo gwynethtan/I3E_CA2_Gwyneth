8269f632d17b6d4501598ad4e4f8987abc3ebe2a
gwynethtan <S10257317@connect.np.edu.sg>
2024-05-26 15:52:11 +0800

    Delete unnecessary AIO pack

--- Assets/Scripts/Player.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour
-{
-    public float movementSpeed = 4.0f;
-    public float jumpHeight = 1.2f;
-}
